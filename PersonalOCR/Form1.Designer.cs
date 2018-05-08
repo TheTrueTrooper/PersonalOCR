f1ca8b498446a79c045a4e9bf05464bc2829ee31
Angelo <Soviet_Trooper@live.ca>
2018-05-07 21:24:29 -0600

    Firs commit
    First Commit Rips basic PDF text, and Filps PDFs (so far) - Day 1

---
+++ PersonalOCR/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PersonalOCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RiTe_View = new System.Windows.Forms.RichTextBox();
+            this.Bu_LoadIn = new System.Windows.Forms.Button();
+            this.Bu_Prev = new System.Windows.Forms.Button();
+            this.Bu_Next = new System.Windows.Forms.Button();
+            this.La_PageNumber = new System.Windows.Forms.Label();
+            this.Bu_FlipPDF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RiTe_View
+            // 
+            this.RiTe_View.Location = new System.Drawing.Point(14, 3);
+            this.RiTe_View.Name = "RiTe_View";
+            this.RiTe_View.ReadOnly = true;
+            this.RiTe_View.Size = new System.Drawing.Size(1149, 572);
+            this.RiTe_View.TabIndex = 1;
+            this.RiTe_View.Text = "";
+            // 
+            // Bu_LoadIn
+            // 
+            this.Bu_LoadIn.Location = new System.Drawing.Point(348, 610);
+            this.Bu_LoadIn.Name = "Bu_LoadIn";
+            this.Bu_LoadIn.Size = new System.Drawing.Size(470, 23);
+            this.Bu_LoadIn.TabIndex = 2;
+            this.Bu_LoadIn.Text = "Load PDF";
+            this.Bu_LoadIn.UseVisualStyleBackColor = true;
+            this.Bu_LoadIn.Click += new System.EventHandler(this.BuLoadIn_Click);
+            // 
+            // Bu_Prev
+            // 
+            this.Bu_Prev.Enabled = false;
+            this.Bu_Prev.Location = new System.Drawing.Point(3, 610);
+            this.Bu_Prev.Name = "Bu_Prev";
+            this.Bu_Prev.Size = new System.Drawing.Size(339, 23);
+            this.Bu_Prev.TabIndex = 3;
+            this.Bu_Prev.Text = "Prev Page";
+            this.Bu_Prev.UseVisualStyleBackColor = true;
+            this.Bu_Prev.Click += new System.EventHandler(this.Bu_Prev_Click);
+            // 
+            // Bu_Next
+            // 
+            this.Bu_Next.Enabled = false;
+            this.Bu_Next.Location = new System.Drawing.Point(824, 610);
+            this.Bu_Next.Name = "Bu_Next";
+            this.Bu_Next.Size = new System.Drawing.Size(339, 23);
+            this.Bu_Next.TabIndex = 4;
+            this.Bu_Next.Text = "Next Page";
+            this.Bu_Next.UseVisualStyleBackColor = true;
+            this.Bu_Next.Click += new System.EventHandler(this.Bu_Next_Click);
+            // 
+            // La_PageNumber
+            // 
+            this.La_PageNumber.AutoSize = true;
+            this.La_PageNumber.Location = new System.Drawing.Point(11, 584);
+            this.La_PageNumber.Name = "La_PageNumber";
+            this.La_PageNumber.Size = new System.Drawing.Size(93, 13);
+            this.La_PageNumber.TabIndex = 5;
+            this.La_PageNumber.Text = "Page Number Null";
+            // 
+            // Bu_FlipPDF
+            // 
+            this.Bu_FlipPDF.Location = new System.Drawing.Point(824, 581);
+            this.Bu_FlipPDF.Name = "Bu_FlipPDF";
+            this.Bu_FlipPDF.Size = new System.Drawing.Size(339, 23);
+            this.Bu_FlipPDF.TabIndex = 6;
+            this.Bu_FlipPDF.Text = "Flip PDF";
+            this.Bu_FlipPDF.UseVisualStyleBackColor = true;
+            this.Bu_FlipPDF.Click += new System.EventHandler(this.Bu_FlipPDF_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1171, 642);
+            this.Controls.Add(this.Bu_FlipPDF);
+            this.Controls.Add(this.RiTe_View);
+            this.Controls.Add(this.Bu_LoadIn);
+            this.Controls.Add(this.Bu_Prev);
+            this.Controls.Add(this.Bu_Next);
+            this.Controls.Add(this.La_PageNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox RiTe_View;
+        private System.Windows.Forms.Button Bu_LoadIn;
+        private System.Windows.Forms.Button Bu_Prev;
+        private System.Windows.Forms.Button Bu_Next;
+        private System.Windows.Forms.Label La_PageNumber;
+        private System.Windows.Forms.Button Bu_FlipPDF;
+    }
+}
+
---
+++ PersonalOCR/Form1.cs
@@ -0,0 +1,140 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PersonalOCR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        const string La_PagerNumberQuick = "Page Number ";
+        const string FileTypes = "PDF Files(*.PDF)| *.PDF| ALL Files(*.*)| *.*";
+
+        List<string> _PagesText;
+
+        int _PageCount;
+        int _Page;
+
+
+        private void BuLoadIn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog FileFinder = new OpenFileDialog();
+            FileFinder.Filter = FileTypes;
+
+            if (FileFinder.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (PdfReader PDF = new PdfReader(FileFinder.FileName))
+                    {
+                        
+
+                        _PagesText = new List<string>(PDF.NumberOfPages);
+                        _PageCount = PDF.NumberOfPages;
+                        for (int P = 0; P < PDF.NumberOfPages; P++)
+                        {
+                            
+                            ITextExtractionStrategy ExtractionStrategy = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+                            string PageText = PdfTextExtractor.GetTextFromPage(PDF, P + 1, ExtractionStrategy);
+                            // Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(PageText));//why but keeping it for now
+                            _PagesText.Add(PageText);
+                        }
+
+                        _Page = 0;
+                        Bu_Prev.Enabled = false;
+
+                        La_PageNumber.Text = La_PagerNumberQuick + 1;
+                        if (_PageCount > 1)
+                            Bu_Next.Enabled = true;
+                        else
+                            Bu_Next.Enabled = false;
+
+                        RiTe_View.Text = _PagesText[_Page];
+                    }
+                }
+                catch (Exception Exp)
+                {
+                    MessageBox.Show(Exp.Message);
+                }
+
+            }
+
+        }
+
+        private void Bu_Next_Click(object sender, EventArgs e)
+        {
+            if (_Page < _PageCount - 1)
+            {
+                _Page++;
+                RiTe_View.Text = _PagesText[_Page];
+                La_PageNumber.Text = La_PagerNumberQuick + (_Page + 1);
+                Bu_Prev.Enabled = true;
+                if (_Page == _PageCount - 1)
+                {
+                    Bu_Next.Enabled = false;
+                }
+            }
+        }
+
+        private void Bu_Prev_Click(object sender, EventArgs e)
+        {
+            if (_Page >= 0)
+            {
+                _Page--;
+                RiTe_View.Text = _PagesText[_Page];
+                La_PageNumber.Text = La_PagerNumberQuick + (_Page + 1);
+                Bu_Next.Enabled = true;
+                if (_Page == 0)
+                {
+                    Bu_Prev.Enabled = false;
+                }
+            }
+        }
+
+        private void Bu_FlipPDF_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog FileFinder = new OpenFileDialog();
+            FileFinder.Filter = FileTypes;
+
+            if (FileFinder.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (PdfReader PDF = new PdfReader(FileFinder.FileName))
+                    {
+                        for (int P = 0; P < PDF.NumberOfPages; P++)
+                        {
+                            PDF.GetPageN(P + 1).Put(PdfName.ROTATE, new PdfNumber(180));
+
+                        }
+
+                        PdfStamper stamper = new PdfStamper(PDF, new FileStream(FileFinder.FileName.Insert(FileFinder.FileName.Length - 4, "_PDFFliped"), FileMode.Create));
+                        stamper.Close();
+
+                    }
+                }
+                catch (Exception Exp)
+                {
+                    MessageBox.Show(Exp.Message);
+                }
+
+            }
+        }
+    }
+
+}
+
